d148f2ca24b53a388b84f9681bcdae6712fc4a44
Alex Kress <alexfrequency10@gmail.com>
2022-04-09 18:21:18 -0500

    added function to generate password, added confirmations for character types, concatenated character types, completed function and finished script.js

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,54 @@ var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["#", "@", "%", "!", "&", "$"];
 
+function generatePassword() {
+  var combinedCharacters = [];
+
+  // ask the user how many characters they want in the password
+  var passwordLength = prompt("How many characters would you like your password to contain? Password must contain between 8 and 128 characters.");
+  // check that the entered password length is within the specified limits
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must choose a password length between 8 and 128 characters.");
+    return "";
+}
+  // ask if they want to use upper case letters
+  var useUpperCaseLetters = confirm("Would you like your password to contain uppercase letters?");
+  // ask if they want to use lower case letters
+  var useLowerCaseLetters = confirm("Would you like your password to contain lowercase letters?");
+  // ask if they want to use numbers
+  var useNumbers = confirm("Would you like your password to contain numbers?");
+  // ask if they want to use special characters
+  var useSpecialCharacters = confirm("Would you like your password to contain special characters?")
+  // adding upperCaseLetters array to the combinedCharacters array so that upperCaseLetters are used in the generated password
+  if (useUpperCaseLetters) {
+    combinedCharacters = combinedCharacters.concat(upperCaseLetters);
+  }
+  // adding lowerCaseLetters array to the combinedCharacters array so that upperCaseLetters are used in the generated password
+  if (useLowerCaseLetters) {
+    combinedCharacters = combinedCharacters.concat(lowerCaseLetters);
+  }
+  // adding numbers array to the combinedCharacters array so that upperCaseLetters are used in the generated password
+  if (useNumbers) {
+    combinedCharacters = combinedCharacters.concat(numbers);
+  }
+  // adding special characters array to the combinedCharacters array so that upperCaseLetters are used in the generated password
+  if (useSpecialCharacters) {
+    combinedCharacters = combinedCharacters.concat(specialCharacters);
+  }
+  if (combinedCharacters.length === 0) {
+    alert("You have to choose at least one type of character for your password");
+    return "";
+  }
+  var randomPassword = ""
+  // generate password
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * combinedCharacters.length);
+    randomPassword = randomPassword + combinedCharacters [randomIndex]
+  }
+
+  // return random password
+  return randomPassword;
+}
 
 
 
